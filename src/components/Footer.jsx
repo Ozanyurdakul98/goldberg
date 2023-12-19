5b33837f1a64b970b24d1340eddec2d8316b798c
Leon Yurdakul <ozan.yurdakul58@gmail.com>
2023-12-19 13:23:54 +0100

    finish

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ export function Footer() {
                  <NavLink href="/kontakt">Kontakt</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
-                &copy; {new Date().getFullYear()} Spencer Sharp. All rights
+                &copy; {new Date().getFullYear()} Goldberg Catering. All rights
                 reserved.
               </p>
             </div>
