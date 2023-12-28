65928fecb092d77afa80d56b1a86699b466345ed
Leon Yurdakul <ozan.yurdakul58@gmail.com>
2023-12-28 12:01:10 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -57,7 +57,7 @@ export function Footer() {
         <nav className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
           {navigation.main.map((item) => (
             <div key={item.name} className="pb-6">
-              <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900">
+              <a href={item.href} className="text-sm leading-6 text-gray-600 hover:text-gray-900 hover:dark:text-gray-700">
                 {item.name}
               </a>
             </div>
