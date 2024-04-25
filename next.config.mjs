7c534a29e1b73d86028bebcab10ec37027917459
Leon Yurdakul <ozan.yurdakul58@gmail.com>
2024-04-25 17:44:02 +0200

    ag

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,22 @@ import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+    pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            // Reduce the memory usage for the build process
+            config.optimization.minimize = false
+        }
+        return config
+    },
 }
 
 const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [rehypePrism],
-  },
+    extension: /\.mdx?$/,
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [rehypePrism],
+    },
 })
 
 export default withMDX(nextConfig)
